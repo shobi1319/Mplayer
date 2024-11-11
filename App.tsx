68a6f74046286758e82455fe8cdf6fc13eeb2d9a
Muhammad Shoaib <muhammadshoaib.bscsf21@iba-suk.edu.pk>
2024-11-11 23:16:38 +0500

    Chnanging name

--- App.tsx
+++ App.tsx
@@ -2,19 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { StatusBar, SafeAreaView ,} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 import NotesListScreen from './NotesListScreen';
 import NoteDetailScreen from './NotesDetailScreen';
 
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-// Make sure to include these imports:
-// import { GoogleGenerativeAI } from "@google/generative-ai";
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-const prompt = "Write a story about a magic backpack.";
-
-const result = await model.generateContent(prompt);
-console.log(result.response.text());
 
 const Stack = createStackNavigator();
 
